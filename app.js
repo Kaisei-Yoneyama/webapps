cbac6be47a61c96d6686f61c5c6fd40d4912cb16
Kaisei Yoneyama <kaisei.yoneyama@gmail.com>
2022-02-01 03:06:49 +0900

    font-src を追加で設定

--- app.js
+++ app.js
@@ -87,8 +87,9 @@ app.use(
   helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: [`'self'`],
+      fontSrc: [`'self`, `cdn.jsdelivr.net`],
       imgSrc: [`'self'`, `cdn.jsdelivr.net`, `${BUCKET}.s3.${REGION}.amazonaws.com`],
-      styleSrc: [`'self'`, 'cdn.jsdelivr.net'],
+      styleSrc: [`'self'`, `cdn.jsdelivr.net`],
       scriptSrc: [`'self'`, `cdn.jsdelivr.net`],
     }
   })
