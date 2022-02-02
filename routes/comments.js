eff5a2f00250de308026d32a62d0382c37dea2f6
Kaisei Yoneyama <kaisei.yoneyama@gmail.com>
2022-02-02 23:47:45 +0900

    コメントの投稿・削除でバリデーションを行う

--- routes/comments.js
+++ routes/comments.js
@@ -6,22 +6,28 @@ const ensureAuthenticated = require('./ensure');
 const Comment = require('../models/comment');
 const csrf = require('csurf');
 const csrfProtection = csrf({ cookie: true });
+const { body, param, query, validationResult } = require('express-validator');
 
 // 投稿
 router.post(
   '/:applicationId/comments',
   ensureAuthenticated,
   csrfProtection,
+  param('applicationId').notEmpty().isUUID(),
+  body('comment').notEmpty().isLength({ max: 200 }),
   (req, res, next) => {
-    if (req.body.comment) {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
       Comment.create({
         userId: req.user.userId,
         applicationId: req.params.applicationId,
-        comment: req.body.comment.slice(0, 200)
+        comment: req.body.comment
       }).then((comment) => {
+        console.info('コメントが投稿されました', comment);
         res.redirect(`/applications/${comment.applicationId}`);
       }).catch(next);
     } else {
+      console.error('コメントの投稿に失敗しました', errors);
       res.redirect(`/applications/${req.params.applicationId}`);
     }
   }
@@ -32,16 +38,22 @@ router.post(
   '/:applicationId/comments/:commentId',
   ensureAuthenticated,
   csrfProtection,
+  param('applicationId').notEmpty().isUUID(),
+  param('commentId').notEmpty().isNumeric(),
+  query('delete').notEmpty().isWhitelisted('1'),
   (req, res, next) => {
-    if (parseInt(req.query.delete) === 1) {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
       Comment.findByPk(req.params.commentId).then((comment) => {
         if (comment && comment.userId === req.user.userId) {
           comment.destroy().then(() => {
+            console.info('コメントが削除されました', comment);
             res.redirect(`/applications/${req.params.applicationId}`);
           }).catch(next);
         }
       });
     } else {
+      console.error('コメントの削除に失敗しました', errors)
       res.redirect(`/applications/${req.params.applicationId}`);
     }
   }
