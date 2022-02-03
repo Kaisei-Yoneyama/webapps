7477140913961f3cf91bdd1063c38e628c0367ff
Kaisei Yoneyama <kaisei.yoneyama@gmail.com>
2022-02-04 02:28:02 +0900

    投稿のバリデーションを追加

--- routes/applications.js
+++ routes/applications.js
@@ -24,83 +24,132 @@ const csrfProtection = csrf({ cookie: true });
 const marked = require('marked');
 
 const multer = require('multer');
+
 const upload = multer({
   storage: multer.memoryStorage(),
-  limits: {fileSize: 1024 ** 2},
+  limits: { fileSize: 1024 ** 2 },
   fileFilter: (req, file, cb) => {
     return /image/.test(file.mimetype) ? cb(null, true) : cb(new Error('Unsupported file type'));
   }
 }).single('thumbnail');
 
+function uploader(req, res, next) {
+  upload(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      res.locals.multerError = err;
+      return next();
+    } else if (err) {
+      res.locals.error = err;
+      return next();
+    } else {
+      // express-validator で使用するために req.body に格納する
+      req.body.file = req.file;
+      return next();
+    }
+  });
+}
+
+function validator(value, { req, location, path }) {
+  if (req.res.locals.multerError instanceof multer.MulterError) {
+    throw req.res.locals.multerError;
+  } else if (req.res.locals.error instanceof Error) {
+    throw req.res.locals.error;
+  } else if (value === undefined) {
+    throw new Error('File does not exist');
+  } else {
+    return true;
+  }
+}
+
+const { body, param, query, validationResult } = require('express-validator');
+const createError = require('http-errors');
+
 // 投稿ページを表示する
 router.get('/new', ensureAuthenticated, csrfProtection, (req, res, next) => {
-  res.render('new', {user: req.user, csrfToken: req.csrfToken()});
+  res.render('new', { user: req.user, csrfToken: req.csrfToken() });
 });
 
 // 投稿を保存する
 router.post('/',
   ensureAuthenticated,
   csrfProtection,
+  uploader,
+  body('file')
+    .custom(validator),
+  body('name')
+    .notEmpty().withMessage('必須入力項目です')
+    .isLength({ max: 100 }).withMessage('100 字以内で入力してください'),
+  body('description')
+    .notEmpty().withMessage('必須入力項目です')
+    .isLength({ max: 1000 }).withMessage('1000 字以内で入力してください'),
+  body('repository')
+    .notEmpty().withMessage('必須入力項目です')
+    .isURL().withMessage('正しい URL を入力してください')
+    .isLength({ max: 100 }).withMessage('100 字以内で入力してください'),
+  body('url')
+    .notEmpty().withMessage('必須入力項目です')
+    .isURL().withMessage('正しい URL を入力してください')
+    .isLength({ max: 100 }).withMessage('100 字以内で入力してください'),
   (req, res, next) => {
-    upload(req, res, (err) => {
-      if (err instanceof multer.MulterError) {
-        return res.render('new', {user: req.user, data: req.body, error: err, csrfToken: req.csrfToken()});
-      } else if (err) {
-        return res.render('new', {user: req.user, data: req.body, error: err, csrfToken: req.csrfToken()});
-      } 
-
-      if (req.body.name && req.body.description && req.body.repository && req.body.url && req.file) {
-        s3Client.putObject(req.file.buffer).then((file) => {
-          Application.create({
-            applicationId: uuid.v4(),
-            applicationName: req.body.name.slice(0, 100),
-            applicationDescription: req.body.description.slice(0, 1000),
-            applicationRepository: req.body.repository.slice(0, 100),
-            applicationUrl: req.body.url.slice(0, 100),
-            applicationThumbnail: file.url,
-            userId: req.user.userId
-          }).then((application) => {
-            return res.redirect(`/applications/${application.applicationId}`);
-          }).catch(next);
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      s3Client.putObject(req.file.buffer).then((file) => {
+        Application.create({
+          applicationId: uuid.v4(),
+          applicationName: req.body.name,
+          applicationDescription: req.body.description,
+          applicationRepository: req.body.repository,
+          applicationUrl: req.body.url,
+          applicationThumbnail: file.url,
+          userId: req.user.userId
+        }).then((application) => {
+          return res.redirect(`/applications/${application.applicationId}`);
         }).catch(next);
-      } else {
-        const error = new Error('入力必須項目に入力されていません');
-        return res.render('new', { user: req.user, error });
-      }
-    });
+      }).catch(next);
+    } else {
+      return res.render('new', {
+        user: req.user,
+        errors: errors.errors,
+        csrfToken: req.csrfToken()
+      });
+    }
   });
 
 // 表示
 router.get(
   '/:applicationId',
   csrfProtection,
+  param('applicationId').notEmpty().isUUID(),
   (req, res, next) => {
-    Application.findOne({
-      include: [{ model: User, attributes: ['userId', 'userName', 'displayName'] }],
-      where: { applicationId: req.params.applicationId }
-    }).then((application) => {
-      if (application === null) {
-        const error = new Error('指定されたページは存在しません');
-        error.status = 404;
-        return next(error);
-      }
-      Comment.findAll({
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      Application.findOne({
         include: [{ model: User, attributes: ['userId', 'userName', 'displayName'] }],
-        where: { applicationId: application.applicationId },
-        order: [['commentId', 'DESC']]
-      }).then((comments) => {
-        comments.forEach((comment) => {
-          comment.formattedCreatedAt = dayjs(comment.createdAt).tz('Asia/Tokyo').format('YYYY年MM月DD日 HH時mm分ss秒');
-          comment.formattedUpdatedAt = dayjs(comment.updatedAt).tz('Asia/Tokyo').format('YYYY年MM月DD日 HH時mm分ss秒');
-        });
-        return res.render('application', {
-          csrfToken: req.csrfToken(),
-          application: application,
-          comments: comments,
-          user: req.user
-        });
+        where: { applicationId: req.params.applicationId }
+      }).then((application) => {
+        if (application === null) {
+          return next(createError(404));
+        }
+        Comment.findAll({
+          include: [{ model: User, attributes: ['userId', 'userName', 'displayName'] }],
+          where: { applicationId: application.applicationId },
+          order: [['commentId', 'DESC']]
+        }).then((comments) => {
+          comments.forEach((comment) => {
+            comment.formattedCreatedAt = dayjs(comment.createdAt).tz('Asia/Tokyo').format('YYYY年MM月DD日 HH時mm分ss秒');
+            comment.formattedUpdatedAt = dayjs(comment.updatedAt).tz('Asia/Tokyo').format('YYYY年MM月DD日 HH時mm分ss秒');
+          });
+          return res.render('application', {
+            csrfToken: req.csrfToken(),
+            application: application,
+            comments: comments,
+            user: req.user
+          });
+        }).catch(next);
       }).catch(next);
-    }).catch(next);
+    } else {
+      res.redirect('/');
+    }
   });
 
 // 編集
@@ -108,19 +157,23 @@ router.get(
   '/:applicationId/edit',
   ensureAuthenticated,
   csrfProtection,
+  param('applicationId').notEmpty().isUUID(),
   (req, res, next) => {
-    Application.findOne({
-      include: [{ model: User, attributes: ['userId', 'userName', 'displayName'] }],
-      where: { applicationId: req.params.applicationId }
-    }).then((application) => {
-      if (application && parseInt(application.userId) === parseInt(req.user.userId)) {
-        return res.render('edit', { application, user: req.user, csrfToken: req.csrfToken() });
-      } else {
-        const error = new Error('指定されたアプリがない、または編集する権限がありません');
-        error.status = 404;
-        return next(error);
-      }
-    }).catch(next);
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      Application.findOne({
+        include: [{ model: User, attributes: ['userId', 'userName', 'displayName'] }],
+        where: { applicationId: req.params.applicationId }
+      }).then((application) => {
+        if (application && parseInt(application.userId) === parseInt(req.user.userId)) {
+          return res.render('edit', { application, user: req.user, csrfToken: req.csrfToken() });
+        } else {
+          return next(createError(404));
+        }
+      }).catch(next);
+    } else {
+      next(createError(404));
+    }
   });
 
 // 更新・削除
@@ -180,14 +233,5 @@ router.post(
       }
     }).catch(next);
   });
-
-// 汎用エラーハンドラ
-// router.use((err, req, res, next) => {
-//   if (err instanceof multer.MulterError) {
-//     console.error('MulterError', err.code);
-//   } else {
-//     next(err);
-//   }
-// });
 
 module.exports = router;
