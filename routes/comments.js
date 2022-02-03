0efcaa9b88a09fff43d9675afec354906daeb6ec
Kaisei Yoneyama <kaisei.yoneyama@gmail.com>
2022-02-04 02:24:52 +0900

    投稿・削除ログを削除

--- routes/comments.js
+++ routes/comments.js
@@ -23,11 +23,9 @@ router.post(
         applicationId: req.params.applicationId,
         comment: req.body.comment
       }).then((comment) => {
-        console.info('コメントが投稿されました', comment);
         res.redirect(`/applications/${comment.applicationId}`);
       }).catch(next);
     } else {
-      console.error('コメントの投稿に失敗しました', errors);
       res.redirect(`/applications/${req.params.applicationId}`);
     }
   }
@@ -38,22 +36,20 @@ router.post(
   '/:applicationId/comments/:commentId',
   ensureAuthenticated,
   csrfProtection,
+  query('delete').notEmpty().isWhitelisted('1'),
   param('applicationId').notEmpty().isUUID(),
   param('commentId').notEmpty().isNumeric(),
-  query('delete').notEmpty().isWhitelisted('1'),
   (req, res, next) => {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
       Comment.findByPk(req.params.commentId).then((comment) => {
         if (comment && comment.userId === req.user.userId) {
           comment.destroy().then(() => {
-            console.info('コメントが削除されました', comment);
             res.redirect(`/applications/${req.params.applicationId}`);
           }).catch(next);
         }
       });
     } else {
-      console.error('コメントの削除に失敗しました', errors)
       res.redirect(`/applications/${req.params.applicationId}`);
     }
   }
